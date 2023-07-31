642d9752da6d8dc7bf4da793078d742ea2c36e4b
andres <aperez@kdu.cl>
2023-07-31 19:48:29 -0400

    refactor

--- internal/managedsecret/handlerconfig.go
+++ internal/managedsecret/handlerconfig.go
@@ -2,7 +2,6 @@ package managedsecret
 
 import (
 	"smanager/internal/httputils"
-	httputil "smanager/internal/httputils"
 	"smanager/internal/middleware"
 )
 
@@ -10,11 +9,11 @@ func NewHandlerConfigProvider(loginService IManagedSecretService) httputils.Hand
 	mh := &ManagedSecretHandler{loginService}
 	middlewares := []middleware.MiddlewareType{middleware.Secured}
 	return &httputils.HandlerProviderBase{
-		HandlersConfigs: []httputil.HandlerConfig{
-			{Route: "/managedSecret", Method: httputil.POST, Handler: mh.CreateManagedSecret, MiddlewareTypes: middlewares},
-			{Route: "/managedSecret", Method: httputil.GET, Handler: mh.ListManagedSecret, MiddlewareTypes: middlewares},
-			{Route: "/managedSecret", Method: httputil.PUT, Handler: mh.EditManagedSecret, MiddlewareTypes: middlewares},
-			{Route: "/managedSecret/:name", Method: httputil.GET, Handler: mh.GetSecret, MiddlewareTypes: middlewares},
+		HandlersConfigs: []httputils.HandlerConfig{
+			{Route: "/managedSecret", Method: httputils.POST, Handler: mh.CreateManagedSecret, MiddlewareTypes: middlewares},
+			{Route: "/managedSecret", Method: httputils.GET, Handler: mh.ListManagedSecret, MiddlewareTypes: middlewares},
+			{Route: "/managedSecret", Method: httputils.PUT, Handler: mh.EditManagedSecret, MiddlewareTypes: middlewares},
+			{Route: "/managedSecret/:name", Method: httputils.GET, Handler: mh.GetSecret, MiddlewareTypes: middlewares},
 		},
 	}
 }
